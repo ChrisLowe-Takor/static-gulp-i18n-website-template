3156777240f39c8d4c0f6b266b51543232368f58
Chris Lowe <chris@takor.com.au>
2018-08-16 12:44:36 +0800

    Improves gulpfile and updates README

--- README.md
+++ README.md
@@ -4,7 +4,8 @@
 This is a gulp script for a static website. It:
  - Handles internationalisation
  - Minifys HTML, CSS, JS and image assets
- - Auto syncs browser
+ - Cache squashes files so the CDN will behave correctly 
+ - Auto syncs browser and reloads changed files while developing
 
 
 ## Getting started
@@ -17,11 +18,29 @@ This is a gulp script for a static website. It:
 
 This will host the english version locally and keep the browser synced with changes made to the html, css, js, translation files and images
 
-Alternatively, you can host one of the translations with:
+Alternatively, you can build, host and sync one of the localizations with:
 
-`gulp sync-zh`
+`gulp --lang=zh`
 
-`gulp sync-pseduo`
+`gulp --lang=pseudo`
+
+You can build the localized versions of the site without browser-sync with:
+
+`gulp build --lang=en`
+`gulp build --lang=zh`
+`gulp build --lang=pseudo`
+
+
+## Working with localized strings
+
+When adding a new string to the html, use the following syntax to identify the string:
+
+`${{ index.MyString }}$`
+
+Add the definition to the JSON in `locale/en/index.json`
+
+`"MyString": "My String!"`
+
 
 ## Project structure
 
@@ -38,44 +57,27 @@ Alternatively, you can host one of the translations with:
    - zh
      - index.json
   - src
-     - img
-     - js
-     - styles
+     - /img
+     - /js
+     - /partials
+     - /styles
+     - /vendor
      - index.html
 ```
 
-
-## Working with this template
-
-When adding a new string to the html, use the following syntax to identify the string:
-
-`${{ MyString }}$`
-
-Add the definition to the JSON in `locale/en/index.json`
-
-`"MyString": "My String!"`
-
-The other translations in the locale directory will fall back to the English definition if they are not defined.
 
 ## Adding new translations
 
 Let's say you want to create a Korean translation (ko).  
 
-- Send a copy of your English json file off to your translator to translate the values
-- Create the directory with the language code in locale and place the new .json translation file.  Eg  `locale/ko/index.json`
-- Open gulpfile.js and add it to the `buildDirs` variable. Ie:
-    ```
-	var buildDirs = [
-	   'build/en/',
-	   'build/zh/',
-	   'build/pseudo/',
-	   'build/ko'
-	];
-	```
-
- - If all goes well you will have a new build directory with the translated website. To see it in action you should copy one of the existing sync tasks such as `sync-zh` to `synch-ko` and change the `baseDir`.
+- Send a copy of your English json file off to your translator
+- Create the directory with the language code in locale and place in the new .json translation file.  Eg  `locale/ko/index.json`
+- You can then build and sync with `gulp --lang=ko`
+- Run `gulp build --lang=ko` and you will have the new translated build under `build/ko`. 
 
 
+## Limitations
 
-
+ - Trying to call `gulp build --lang=xx` where the `xx` localization doesn't exist will fill the build with garbage localizations
+ - If a string is missing in one of the localizations it will fill the missing string with garbage but it will warn you in the console of the missing file.
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
-var gulp = require('gulp');
+
+var gulp = require('gulp-param')(require('gulp'), process.argv);
 var del = require('del')
 var minifyHtml = require("gulp-minify-html");
 var minifyCss = require("gulp-clean-css");
@@ -6,67 +7,81 @@ var minifyJs = require("gulp-minify");
 var livereload = require('gulp-livereload');
 var imagemin = require('gulp-imagemin');
 var i18n = require('gulp-html-i18n');
-var browserSync = require('browser-sync').create();
+var browserSync = require('browser-sync');
 var cache = require('gulp-cache');
-var reload      = browserSync.reload;
+var fileInclude = require('gulp-file-include');
+var cachebust = require('gulp-cache-bust');
+var changedInPlace = require('gulp-changed-in-place');
+var gulpif = require('gulp-if');
+var nop = require('gulp-nop');
+var runSequence = require('run-sequence');
+var reload = browserSync.reload;
 
 
-var buildDirs = [
-   'build/en/',
-   'build/zh/',
-   'build/pseudo/'
-];
+var localization="en";
+
+gulp.task('locale-check', function(lang) {
+   if (lang === undefined) {
+      localization = "en"
+   } else {
+      localization = lang;
+   }
+   return gulp.src('.').pipe(nop());
+});
 
 gulp.task('clean', function() {
   del(['build/*'])
 });
 
+gulp.task("build-vendor", function() {
+   console.log(localization);
+   return gulp.src("src/vendor/*/**")
+      .pipe(changedInPlace( { firstPass: true }))
+      .pipe(gulp.dest("build/" + localization + "/vendor"));
+});
+
 gulp.task("build-css", function() {
-   var pipeline = gulp.src("src/styles/*.css");
-
-   buildDirs.forEach(function(d) {
-      pipeline = pipeline.pipe(minifyCss()).pipe(gulp.dest(d + "/styles/"));
-   });
-
-   return pipeline;
+   return gulp.src("src/css/*.css")
+      .pipe(changedInPlace( { firstPass: true }))
+      .pipe(gulp.dest("build/" + localization + "/css"));
 });
 
 gulp.task("build-js", function() {
-   var pipeline = gulp.src("src/js/*.js");
-
-   buildDirs.forEach(function(d) {
-      pipeline = pipeline.pipe(minifyJs()).pipe(gulp.dest(d + "/js/"));
-   });
-
-   return pipeline;
+   return gulp.src("src/js/*.js")
+      .pipe(changedInPlace( { firstPass: true }))
+      .pipe(gulp.dest("build/" + localization + "/js"));
 });
 
 gulp.task("build-img", function() {
-   var pipeline = gulp.src("src/img/*");
-
-   buildDirs.forEach(function(d) {
-      pipeline = pipeline.pipe(imagemin({ progressive: true, interlaced: true })).pipe(gulp.dest(d + "/img/"));
-   });
+   return gulp.src("src/img/*")
+      .pipe(changedInPlace( { firstPass: true }))
+      .pipe(cache(imagemin({ progressive: true, interlaced: true })))
+      .pipe(gulp.dest("build/" + localization + "/img"));
 });
 
-gulp.task('build-html', function() {
-   gulp.src("src/*.html")
-      .pipe(minifyHtml())
-      .pipe(gulp.dest('build/'));
-});
-
-gulp.task("translate", function() {
+gulp.task("build-html", function() {
    return gulp.src(['src/*.html'])
-   .pipe(i18n({
-      langDir: './locale',
-      fallback: 'en',
-      createLangDirs: true
-   }))
-   .pipe(minifyHtml())
-   .pipe(gulp.dest('build/'));
+      .pipe(fileInclude({ prefix: '@@', baseDir: '/partials'}))
+      .pipe(i18n({
+         langDir: './locale',
+         fallback: 'en',
+         createLangDirs: false,
+         inline: localization
+      }))
+      .pipe(changedInPlace( { firstPass: true }))
+      .pipe(minifyHtml())
+      .pipe(cachebust({ type: 'timestamp' }))
+      .pipe(gulp.dest('build/' + localization));
 });
 
-gulp.task('watch-html', ['translate'], function(done) {
+gulp.task('build-docs', function() {
+   return gulp.src("docs/*/**")
+         .pipe(changedInPlace( { firstPass: true }))
+         .pipe(minifyJs())
+         .pipe(gulp.dest("build/" + localization));
+});
+
+gulp.task('watch-html', ['build-html'], function(done) {
    browserSync.reload();
    done();
 });
@@ -77,7 +92,7 @@ gulp.task('watch-css', ['build-css'], function(done) {
 });
 
 gulp.task('watch-js', ['build-js'], function(done) {
-   browserSync.realod();
+   browserSync.reload();
    done();
 });
 
@@ -87,47 +102,32 @@ gulp.task('watch-img', ['build-img'], function(done) {
 });
 
 
-gulp.task('sync-en', ['translate', 'build-css', 'build-js', 'build-img'], function() {
-   browserSync.init({
+gulp.task('build', function(lang) {
+   if (lang === undefined) {
+      localization = "en"
+   } else {
+      localization = lang;
+   }
+   runSequence('build-html', 'build-css', 'build-js', 'build-img', 'build-vendor', 'build-docs');
+})
+
+
+gulp.task('sync', ['locale-check', 'build-html', 'build-css', 'build-js', 'build-img', 'build-vendor', 'build-docs'], function() {
+   var enBrowser = browserSync.create();
+
+   enBrowser.init({
       server: {
-         baseDir: "build/en"
+         baseDir: "build/" + localization
       }
    });
 
-   gulp.watch("locale/*/**", ["watch-html"]);
-   gulp.watch("src/*.html", ["watch-html"]);
-   gulp.watch("src/styles/*.css", ["watch-css"]);
-   gulp.watch("src/js/*.js", ["watch-js"]);
-   gulp.watch("src/img/*", ["watch-img"]);
+   gulp.watch("locale/*/**", ["watch-html", enBrowser.reload]);
+   gulp.watch("src/*.html", ["watch-html", enBrowser.reload]);
+   gulp.watch("src/partials/*.html", ["watch-html"]);
+   gulp.watch("src/css/*.css", ["watch-css", enBrowser.reload]);
+   gulp.watch("src/js/*.js", ["watch-js", enBrowser.reload]);
+   gulp.watch("src/img/*", ["watch-img", enBrowser.reload]);
 });
 
 
-gulp.task('sync-zh', ['translate', 'build-css', 'build-js', 'build-img'], function() {
-   browserSync.init({
-      server: {
-         baseDir: "build/zh"
-      }
-   });
-
-   gulp.watch("locale/*/**", ["watch-html"]);
-   gulp.watch("src/*.html", ["watch-html"]);
-   gulp.watch("src/styles/*.css", ["watch-css"]);
-   gulp.watch("src/js/*.js", ["watch-js"]);
-   gulp.watch("src/img/*", ["watch-img"]);
-});
-
-gulp.task('sync-pseudo', ['translate', 'build-css', 'build-js', 'build-img'], function() {
-   browserSync.init({
-      server: {
-         baseDir: "build/pseudo"
-      }
-   });
-
-   gulp.watch("locale/*/**", ["watch-html"]);
-   gulp.watch("src/*.html", ["watch-html"]);
-   gulp.watch("src/styles/*.css", ["watch-css"]);
-   gulp.watch("src/js/*.js", ["watch-js"]);
-   gulp.watch("src/img/*", ["watch-img"]);
-});
-
-gulp.task('default', ['sync-en']);
+gulp.task('default', ['sync']);
